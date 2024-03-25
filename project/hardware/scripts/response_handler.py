ccf9a974a97bb77f02110b56656622b7c5f0ce4c
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-03-25 13:23:43 -0500

    Create module handler for Chat-GPT responses

---
+++ project/hardware/scripts/response_handler.py
@@ -0,0 +1,20 @@
+import os
+from os.path import join, dirname
+
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv() # bring env variables in for os
+myKey = os.environ(['OpenAI_Key']) # grab openai api key
+
+client = OpenAI(
+    api_key = myKey
+)
+
+response = client.audio.speech.create(
+    model="tts-1",
+    voice="nova",
+    input="Hello world! This is a streaming test.",
+)
+
+response.stream_to_file("sound/output.mp3")
