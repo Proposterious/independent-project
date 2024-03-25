409e5d3f12e46d5aea9ed21bdfe0e46715a43e73
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-03-25 09:16:06 -0500

    Chore: satisfy pylint rules

--- project/hardware/scripts/speech_handler.py
+++ project/hardware/scripts/speech_handler.py
@@ -1,23 +1,23 @@
 """Converts speech (.wav) files to text"""
 import whisper
 
-Model = whisper.load_model("base")
+MODEL = whisper.load_model("base")
 
 def interpret_speech(path: str) -> str:
     # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio(path)
     audio = whisper.pad_or_trim(audio)
 
-    # make log-Mel spectrogram and move to the same device as the Model
-    mel = whisper.log_mel_spectrogram(audio).to(Model.device)
+    # make log-Mel spectrogram and move to the same device as the MODEL
+    mel = whisper.log_mel_spectrogram(audio).to(MODEL.device)
 
     # detect the spoken language
-    _, probs = Model.detect_language(mel)
+    _, probs = MODEL.detect_language(mel)
     print(f"Detected language: {max(probs, key=probs.get)}")
 
     # decode the audio
     options = whisper.DecodingOptions()
-    result = whisper.decode(Model, mel, options)
+    result = whisper.decode(MODEL, mel, options)
 
     # print the recognized text
     print(result.text)
@@ -29,4 +29,4 @@ async def convert_speech(path: str) -> str:
     path: path to audio file
     """
     
-    text = await Model.transcribe(path)
+    text = await MODEL.transcribe(path)
