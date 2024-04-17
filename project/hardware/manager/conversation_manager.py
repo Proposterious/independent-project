ac92a72c9125fb728d0870b9b931d32104928d7b
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-04-16 23:10:53 -0500

    Update class with `create_speech`, `transcribe_speech` methods

--- project/hardware/manager/conversation_manager.py
+++ project/hardware/manager/conversation_manager.py
@@ -1,17 +1,18 @@
 """ConversationManager Class"""
 import os
+import openai
 from openai import OpenAI
+from pathlib import Path
 from dotenv import load_dotenv
-
-client = OpenAI() # initialize openai connection
 load_dotenv() # bring env variables in for os
 
-OPENAI_KEY = os.environ(['OPENAI_KEY']) # grab api key
-
+OPENAI_KEY = os.environ['OPENAI_API_KEY'] # grab api key
+client = OpenAI(api_key=OPENAI_KEY) # initialize openai connection
 class ConversationManager:
     """Manages User-ChatGPT communication"""
-    def __init__(self, model_name="gpt-3.5-turbo-0125", ai_key = OPENAI_KEY):
+    def __init__(self, model_name="gpt-3.5-turbo-0125", ai_key = OPENAI_KEY, voice = "nova"):
         # Initialize OpenAI API with model
+        self.voice = voice
         self.ai_key = ai_key
         self.model_name = model_name
 
@@ -19,7 +20,7 @@ class ConversationManager:
         """Generate response using OpenAI API"""
 
         response = client.chat.completions.create(
-            model = "gpt-3.5-turbo",
+            model = self.model_name,
             response_format = { "type": "json_object" },
             messages = [
                 user_input
@@ -27,4 +28,29 @@ class ConversationManager:
         )
         return response.choices[0].text.strip()
 
+    def create_speech(self, text, path: str) -> bool:
+        """Convert text to speech and save to file with 'path'"""
+
+        speech_file_path = Path(__file__).parent.parent / "sound" / path
+        response = openai.audio.speech.create(
+            model="tts-1",
+            voice=self.voice,
+            input=text,
+            response_format="wav"
+        )
+
+        response.with_streaming_response.method(speech_file_path)
+
+    def transcribe_speech(self, path) -> bool:
+        """Convert speech to text accessed with 'path'"""
+
+        audio_file = open(path, "rb")
+        transcript = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=audio_file
+        )
+
+        if transcript:
+            return True
+        return False
     # Other methods for conversation management...
