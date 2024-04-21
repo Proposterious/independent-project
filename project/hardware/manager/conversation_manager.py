9f9a8739841851313a079d3a247e803886e13200
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-04-21 15:43:59 -0500

    Finalize `transcribe_speech()` and introduce new imports for file handling

--- project/hardware/manager/conversation_manager.py
+++ project/hardware/manager/conversation_manager.py
@@ -2,34 +2,70 @@
 import os
 import time
 import openai
+import whisper
 from openai import OpenAI
 from pathlib import Path
 from dotenv import load_dotenv
+from playsound import playsound
+from utils.text_utils import affirm
+from utils.file_utils import record_audio
+
 load_dotenv() # bring env variables in for os
 
 OPENAI_KEY = os.environ['OPENAI_API_KEY'] # grab api key
 client = OpenAI(api_key=OPENAI_KEY) # initialize openai connection
+
+PARENT_PATH = Path(__file__).parent
+
 class ConversationManager:
     """Manages User-ChatGPT communication"""
-    def __init__(self, assistant: str, model_name="gpt-4.0-turbo", ai_key = OPENAI_KEY, voice = "nova"):
+    def __init__(self, assistant: str, model_name="gpt-4.0-turbo", voice = "nova"):
         # Initialize OpenAI API with model
         self.voice = voice
-        self.ai_key = ai_key
         self.assistant = assistant
         self.model_name = model_name
 
     def create_speech(self, text, path: str) -> bool:
         """Convert text to speech and save to file with 'path'"""
-
-        speech_file_path = Path(__file__).parent.parent / "sound" / path
-        response = openai.audio.speech.create(
+        speech_file_path = PARENT_PATH / "sound" / path
+        response = client.audio.speech.create(
             model="tts-1",
             voice=self.voice,
             input=text,
             response_format="wav"
         )
 
-        response.with_streaming_response.method(speech_file_path)
+        response.stream_to_file(speech_file_path)
+
+        if response:
+            return True
+        else:
+            return False
+
+    def transcribe_speech(self, path) -> str | bool:
+        """Convert speech to text accessed with 'path'"""
+        speech_file_path = f"{Path(__file__)} / sound / {path}"
+        audio_file = open(speech_file_path, "rb")
+        transcription = openai.audio.transcriptions.create(
+            model="whisper-1",
+            format="wav",
+            file=audio_file
+        )
+
+        if transcription:
+            return transcription.text
+        return False
+    
+    def beep(self):
+        """Play a beep to signal to user that the robot is finished speaking"""
+    # Functions that Interact with User
+    def introduce_user(self):
+        """Introduce the User to VISoR"""
+        introduce_path = os.path.dirname(__file__) + "\sound\introduceUser.mp3"
+        playsound(introduce_path)
+        
+        
+
 
     def respond(self, thread_id: str, user_input: str) -> str:
         """Generate response using OpenAI API"""
@@ -47,33 +83,19 @@ class ConversationManager:
             print(f"Received error {e}, Failure")
 
         run = client.beta.threads.runs.create(
-            thread_id = thread.id,
+            thread_id = thread_id,
             assistant_id = self.assistant
         )
 
         while run.status in ("queued", "in_progress"):
             run = client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
+                thread_id=thread_id,
                 run_id=run.id,
             )
             time.sleep(0.25)
 
         messages = client.beta.threads.messages.list(
-            thread_id = thread_id
+            thread_id = thread.id
         )
 
         # self.create_speech(messages.data[0].content[-1].text.value, "latest_response.wav")
-
-    def transcribe_speech(self, path) -> bool:
-        """Convert speech to text accessed with 'path'"""
-
-        audio_file = open(path, "rb")
-        transcript = client.audio.transcriptions.create(
-            model="whisper-1",
-            file=audio_file
-        )
-
-        if transcript:
-            return True
-        return False
-    # Other methods for conversation management...
