95e396ddf83f58477a068c53a10026b00984a41b
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-04-22 14:32:22 -0500

    Make use of `ConversationManager` within `main()`

--- project/hardware/manager/main.py
+++ project/hardware/manager/main.py
@@ -4,32 +4,49 @@ import asyncio
 from data_manager import DataManager
 from conversation_manager import ConversationManager
 
+"""Initialize scripts, classes, hardware"""
+data_manager = DataManager()
+conversation_manager = ConversationManager(assistant="asst_tHhDGtl8tSJIVTrMd95yt9Uk")
 # runs on startup
 async def main():
-    """Initialize scripts, classes, hardware"""
-    data_manager = DataManager()
-    conversation_manager = ConversationManager(assistant="asst_tHhDGtl8tSJIVTrMd95yt9Uk")
-  
-    # Grab User Info
+    # Find or create user
     user_exists = await conversation_manager.introduce_user()
     print("user_exists", user_exists)
     if user_exists is True:
         # Attempt to retrieve and update user data
         user_name = await conversation_manager.get_name()
         if bool(data_manager.set_data(user_name)) is True:
-            return
+            print("Updated session")
+        else:
+            print("Failed to update session")
+            sys.exit()
     elif user_exists is False: # in-progress
        user_name = await conversation_manager.new_user()
        if bool(data_manager.set_data(user_name)) is True:
-            return
+            print("Updated session")
+       else:
+            print("Failed to update session")
+            sys.exit()
     else:
         print("Could not determine if user exists, ", user_exists)
         sys.exit()
 
+    # Send user into loop or update settings
+    begin_session = await conversation_manager.begin_session()
+    if bool(begin_session == "yes"):
+        return
+    else:
+        asyncio.run(options)
+        # ask user if they would like to begin session
+    
 # runs after setup
 async def loop():
     """Serves as repetitive interaction loop"""
     print("blah")
+
+async def options(user):
+    print("blah")
+
 # # Entry point of the application
 # if __name__ == "__main__":
 #     main()
