b19baa6161bbfc8e6bbf7be12314f3364571b654
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-04-23 19:22:37 -0500

    Minor changes: make `OPENAI_API_KEY` globally accessible

--- project/hardware/manager/data_manager.py
+++ project/hardware/manager/data_manager.py
@@ -1,5 +1,13 @@
 """DataManager Class"""
+import os
+import time
+from openai import OpenAI
+from dotenv import load_dotenv
 from utils.file_utils import read_users
+
+load_dotenv() # bring in env variables
+openai_api_Key = os.environ["OPENAI_API_KEY"] # assign api key to constant var
+
 class DataManager:
     """Manages Data communication"""
     def __init__(self, data = None):
@@ -30,7 +38,7 @@ class DataManager:
             return True
         except KeyError:
             return False
-        
+    
     def save_conversation(self, new_line: list) -> bool:
         """ Appends newLine to data.conversation Array """
         length = len(self.data.conversation)
@@ -52,4 +60,39 @@ class DataManager:
         for x in range(1, latest):
             requested.append(self.data.conversation[-x])
         return requested
+  
+    async def manage_threads(self, action, thread: str = None, content: str = None):
+        """ Handles CRUD actions for OpenAI thread objs """
+        client = OpenAI(api_key=openai_api_Key) # initialize client
 
+        if action == "CREATE": # create new thread
+            thread = client.beta.threads.create()
+        elif action == "READ" & thread != None: # retrieve an existing thread
+            thread = client.beta.threads.retrieve(
+                thread_id = thread
+            )
+        elif action == "UPDATE" & thread != None: # update an existing thread
+            run = await client.beta.threads.runs.create(
+                thread_id = thread,
+                assistant_id = self.data.assistant
+            )
+            while run.status in ("queued", "in_progress"):
+                run = client.beta.threads.runs.retrieve(
+                    thread_id=thread.id,
+                    run_id=run.id,
+                )
+                time.sleep(0.2)
+            
+            messages = client.beta.threads.messages.list(
+                thread_id = thread
+            )
+            for message in reversed(messages.data):
+                print(message.role + " : " + message.content[0].text.value)
+    
+        elif action == "DELETE" & content != None: # delete an existing thread
+            thread = client.beta.threads.delete(
+                thread_id = thread
+            )
+        else:
+            print("Invalid action, No action taken")
+    
