9b9c311be0dfc3c6be6ac15e81791dd526e565e1
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-04-25 13:43:32 -0500

    Update `main.py` with paths to session `loop` or edit `options`

--- project/hardware/manager/main.py
+++ project/hardware/manager/main.py
@@ -4,51 +4,57 @@ import asyncio
 from data_manager import DataManager
 from conversation_manager import ConversationManager
 
-"""Initialize scripts, classes, hardware"""
-data_manager = DataManager()
-conversation_manager = ConversationManager(assistant="asst_tHhDGtl8tSJIVTrMd95yt9Uk")
-# runs on startup
-async def main():
-    # Find or create user
-    user_exists = await conversation_manager.introduce_user()
-    print("user_exists", user_exists)
+data_manager = DataManager() # initialize DataManager class
+conversation_manager = ConversationManager(assistant="asst_tHhDGtl8tSJIVTrMd95yt9Uk") # initialize ConversationManager class
+
+def loop(thread) -> None:
+    """Serves as repetitive interaction loop"""
+    while True:
+        user_input = conversation_manager.user_response()
+        response = conversation_manager.assistant_response(user_input, thread)
+        print(response)
+
+def options(user_name: str) -> None:
+    """Accesses and edits user options"""
+    print(user_name)
+
+async def main() -> None:
+    """Initializes the program"""
+    user_exists: bool = await conversation_manager.introduce_user()
+    # Create or find user then update session
     if user_exists is True:
         # Attempt to retrieve and update user data
-        user_name = await conversation_manager.get_name()
+        user_name = conversation_manager.get_name()
         if bool(data_manager.set_data(user_name)) is True:
             print("Updated session")
         else:
             print("Failed to update session")
             sys.exit()
     elif user_exists is False: # in-progress
-       user_name = await conversation_manager.new_user()
-       if bool(data_manager.set_data(user_name)) is True:
+        user_name = await conversation_manager.new_user()
+        data_manager.create_user(user_name)
+        if bool(data_manager.set_data(user_name)) is True:
             print("Updated session")
-       else:
+        else:
             print("Failed to update session")
             sys.exit()
     else:
         print("Could not determine if user exists, ", user_exists)
         sys.exit()
 
-    # Send user into loop or update settings
+    # Send user into loop or options
     begin_session = await conversation_manager.begin_session()
     if bool(begin_session == "yes"):
-        return
+        if user_exists is True:
+            prev_stories = data_manager.data.stories
+            story_index: int = conversation_manager.prev_sessions(prev_stories)
+
+            thread: str = prev_stories[story_index].thread_id
+            asyncio.run(loop(thread))
     else:
-        asyncio.run(options)
+        asyncio.run(options(data_manager.data.name))
         # ask user if they would like to begin session
-    
-# runs after setup
-async def loop():
-    """Serves as repetitive interaction loop"""
-    print("blah")
 
-async def options(user):
-    print("blah")
-
-# # Entry point of the application
-# if __name__ == "__main__":
-#     main()
-asyncio.run(main())
-asyncio.run(loop())
+# Entry point of the application
+if __name__ == "__main__":
+    asyncio.run(main())
