caab9434cce1c98079a125b92babf426e7508424
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-05-01 12:18:42 -0500

    Repurpose `scripts` folder for InMoov robot scripts

--- project/hardware/scripts/runtime.py
+++
@@ -1,9 +0,0 @@
-"""Takes Note of Major Changes during Runtime"""
-
-def timestamp():
-    # Notes time from program start
-    return
-
-def end_program(text: str) -> str:
-    # Specifies what function ended the program
-    return text
