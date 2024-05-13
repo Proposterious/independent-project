82cbf264c8ac98cce4bcb7ef8be64a8d4cfa860d
Proposterious <98131932+Proposterious@users.noreply.github.com>
2024-05-12 21:01:27 -0500

    Check for `trigger_exit` in user response while in `loop`

--- project/hardware/manager/main.py
+++ project/hardware/manager/main.py
@@ -1,6 +1,7 @@
 # Import necessary modules
 import sys
 import asyncio
+from utils.text_utils import trigger_exit
 from data_manager import DataManager
 from conversation_manager import ConversationManager
 
@@ -17,6 +18,9 @@ def loop(thread = None) -> None:
         print("this should provide the user a summary of prev thread/story")
     while True:
         user_input = conversation_manager.user_response()
+        if trigger_exit(user_input):
+            break
+
         response = conversation_manager.assistant_response(user_input, thread)
         print(response)
 
@@ -56,6 +60,7 @@ def main() -> None:
             story_index: int = conversation_manager.prev_sessions(prev_stories)
 
             thread: str = prev_stories[story_index].thread_id
+            conversation_manager.begin_communications(thread)
             asyncio.run(loop(thread))
         else: # user_exists is False
             asyncio.run(loop())
